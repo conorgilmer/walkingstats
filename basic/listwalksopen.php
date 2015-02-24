ca9f3fbe38045c3fb13b87058ce0ccfb5ca2b340
Conor Gilmer <conor.gilmer@gmail.com>
2015-02-24 20:27:35 +0000

    public pages

---
+++ basic/listwalksopen.php
@@ -0,0 +1,113 @@
+<?php
+session_start();
+
+/*
+ * Set up constant to ensure include files cannot be called on their own
+*/
+define ( "MY_APP", 1 );
+/*
+ * Set up a constant to your main application path
+ */
+define ( "APPLICATION_PATH", "application" );
+define ( "TEMPLATE_PATH", APPLICATION_PATH . "/view" );
+
+//include_once(APPLICATION_PATH . "/inc/session.inc.php");
+
+
+/*
+ * Include the config.inc.php file
+ */
+include (APPLICATION_PATH . "/inc/config.inc.php");
+include (APPLICATION_PATH . "/inc/db.inc.php");
+include (APPLICATION_PATH . "/inc/functions.inc.php");
+
+//Set up variable so 'active' class set on navbar link
+$activeHome = "active";
+
+include (TEMPLATE_PATH . "/public/header.html");
+
+?>
+
+<div class="container">
+<div class="row">
+<div class="span12">
+<h1>List Walks</h1>
+</div>
+</div>
+<div clas="row">
+<div class="span9">
+
+<?php 
+
+$sqlQuery = "SELECT * FROM walks";
+$result = mysql_query($sqlQuery);
+
+
+if ($result) {
+	$htmlString = "";
+	$htmlString .=  "<table class='table table-bordered table-condensed table-striped' border='1'>\n";
+	
+	$htmlString .= "<tr>";
+	$htmlString .= "<th>ID</th>";
+	$htmlString .= "<th>Minutes</th>";
+	$htmlString .= "<th>Distance</th>";
+	$htmlString .= "<th>Speed</th>";
+        $htmlString .= "<th>Place</th>";
+        $htmlString .= "<th>Description</th>";
+        $htmlString .= "<th>Date Added</th>";
+	
+	$htmlString .= "</tr>";
+	
+	while ($product = mysql_fetch_assoc($result))
+	{
+		$htmlString .=  "<tr>" ;
+		$htmlString .=  "<td>";
+		$htmlString .=  $product["id"];
+		$htmlString .=  "</td>";
+		$htmlString .=  "<td>";
+		$htmlString .=  $product["minutes"];
+		$htmlString .=  "</td>";
+		$htmlString .=  "<td>";
+		$htmlString .=  $product["distance_km"];
+		$htmlString .=  "</td>";
+		$htmlString .=  "<td>";
+		$htmlString .=  $product["speed"];
+		$htmlString .=  "</td>";
+                $htmlString .=  "<td>";
+		$htmlString .=  getRoute($product["place"]);
+		$htmlString .=  "</td>";
+                
+                		$htmlString .=  "<td>";
+		$htmlString .=  $product["description"];
+		$htmlString .=  "</td>";
+		$htmlString .=  "<td>";
+                $htmlString .=  $product["date"];
+//		$htmlString .=  getCountry($product["date"]);
+		$htmlString .=  "</td>";
+		
+		
+		
+		$htmlString .=  "</tr>\n";
+		
+	}
+	$htmlString .=  "</table>\n";
+	
+	echo $htmlString ;
+	
+	
+	
+} else {
+	
+	die("Failure: " . mysql_error($link_id));
+}
+?>
+</div>
+<div class="span3"></div>
+
+</div>
+
+
+</div> <!-- /container -->
+<?php 
+include (TEMPLATE_PATH . "/footer.html");
+?>
